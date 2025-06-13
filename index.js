41e011cb77a17cd60252f90d3dec5d22c1651dba
rokonmahmud <rokonudden6969@gmail.com>
2025-06-13 22:08:39 +0600

    our cgpa calculator

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const name= 'samia'
+let mark= 78
+if(mark >= 90) {
+    console.log('A+')
+}else if(mark>=80){
+    console.log('A')
+}else if(mark>=70){
+    console.log('A-')
+}else if(mark>=65){
+    console.log('B')
+}else if(mark>=60){
+    console.log('B-')
+}else if(mark>=55){
+    console.log('C+')
+}else if(mark>=50){
+    console.log('c')
+}else if(mark>=45){
+    console.log('C-')
+}else if(mark>=40){
+    console.log('D+')
+}else if(mark>=35){
+    console.log('D')
+}else {
+    console.log('fail')
+}
